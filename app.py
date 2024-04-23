0f4668a9e52efadc753470c6f7e2c5eaf46ff65b
igor <yurakov99@gmail.com>
2024-04-23 12:16:26 +0300

    test after secret

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 
-# from dotenv import find_dotenv, load_dotenv
-# load_dotenv(find_dotenv())
+from dotenv import find_dotenv, load_dotenv
+load_dotenv(find_dotenv())
 
 from handlers.user_private import user_private_router
 from handlers.user_group import user_group_router
@@ -14,7 +14,7 @@ from common.bot_cmds_list import private
 
 ALLOWED_UPDATES = ['message, edited_message']
 
-bot = Bot('SECRET', parse_mode=ParseMode.HTML)
+bot = Bot(token=os.getenv('TOKEN'), parse_mode=ParseMode.HTML)
 dp = Dispatcher()
 
 dp.include_router(user_private_router)
